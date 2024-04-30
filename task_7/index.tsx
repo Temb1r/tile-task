23c7488e29d7d68092d048b4fd6c49dc30501e67
Ivan Piskun <tembir.ivan@gmail.com>
2024-04-30 12:47:28 +0300

    type:feature - task_7 has been completed

--- task_7/index.tsx
+++ task_7/index.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Button(props: { children: string }) {
-    const [disabled, setDisabled] = useState<boolean>();
+  const [disabled, setDisabled] = useState<boolean>();
 
-    return (
-        <button className='' onClick={() => setDisabled(true)} disabled={disabled}>
-            {props.children}
-        </button>
-    );
+  return (
+    <button
+      className={`p-2 rounded-lg text-white bg-blue-700 ${
+        disabled ? "bg-gray-400" : "hover:bg-blue-500"
+      }`}
+      onClick={() => setDisabled(true)}
+      disabled={disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
